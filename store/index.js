1d6ba4b3298de36b580ceed82564b03ac712d358
Kevaron <kevtapia75@gmail.com>
2022-06-04 18:01:35 -0500

    renaming branch to development

--- store/index.js
+++ store/index.js
@@ -1,13 +1,9 @@
 import storiesStore from './stories'
 import productsStore from './products' 
 
-import { database } from '../firebase/firebase'
-import { child, get, ref } from 'firebase/database';
-
 export default {
   modules: {
     stories: storiesStore,
     products: productsStore
   }
 }
-hola soy kevin aron tapia cruz
